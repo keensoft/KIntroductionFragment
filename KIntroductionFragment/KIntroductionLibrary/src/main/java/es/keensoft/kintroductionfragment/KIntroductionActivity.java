7820882ce77f082369801c6b0d2e2450533026a8
David Lago <david.lago@keensoft.es>
2015-03-09 13:58:35 +0100

    Modificaciones Propiedades y eliminacion del menu

--- KIntroductionFragment/KIntroductionLibrary/src/main/java/es/keensoft/kintroductionfragment/KIntroductionActivity.java
+++ KIntroductionFragment/KIntroductionLibrary/src/main/java/es/keensoft/kintroductionfragment/KIntroductionActivity.java
@@ -161,32 +161,7 @@ public abstract class KIntroductionActivity extends ActionBarActivity implements
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_k_introduction, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onPageScrolled(int i, float v, int i2) {
-
-    }
 
     @Override
     public void onPageSelected(int i) {
